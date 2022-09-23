9dbd19a902ea00a8472c746d2e7e8aebf08117a4
JesusAdanOp <jesusadanorozcop@outlook.com>
2022-09-23 08:54:06 -0600

    Agregar archivos de proyecto.

---
+++ Division Con Get Set/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Division_Con_Get_Set
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cldivision div = new cldivision();
+            div.V1 = double.Parse(txtv1.Text);
+            div.V2 = double.Parse(txtv2.Text);
+
+            txtres.Text = div.dividir().ToString();
+        }
+    }
+}
---
+++ Division Con Get Set/cldivision.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Division_Con_Get_Set
+{
+    internal class cldivision
+    {
+        double v1, v2;
+
+        public double V1
+        {
+            get { return v1; }
+            set { v1 = value; }
+        }
+
+        public double V2
+        {
+            get { return v2; }
+            set { v2 = value; }
+        }
+
+        public double dividir()
+        {
+            return v1 / v2;
+        }
+    }
+}
